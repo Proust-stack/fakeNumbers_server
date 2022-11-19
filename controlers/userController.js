4b1b76f339c8b75824fb5adda6c1c23d6ef044c6
Proust-stack <andrey-hunter@yandex.ru>
2022-11-19 23:33:48 +0400

    pages, limit

--- controlers/userController.js
+++ controlers/userController.js
@@ -4,24 +4,55 @@ const createRandomUser = require("../utils/fakeGenerator");
 
 class UserController {
   getData(req, res, next) {
-    const page = req.query.page || "1";
-    const locale = req.query.locale;
+    function modelCreation() {
+      const locale = req.query.locale;
+      const seed = req.query.seed;
 
-    const seed = req.query.seed || "1";
-
-    if (!locale || !seed) {
-      return next(
-        ApiError.badRequest("Incorrect data, fullfill all fields above, please")
-      );
-    }
-    faker.setLocale(locale);
-    faker.seed(+seed + +page);
-    const users = [];
-    Array.from({ length: 5 }).forEach(() => {
-      users.push(createRandomUser());
+      if (!locale || !seed) {
+        return next(
+          ApiError.badRequest(
+            "Incorrect data, fullfill all fields above, please"
+          )
+        );
+      }
+      faker.setLocale(locale);
+      faker.seed(+seed + +page);
+      const users = [];
+      Array.from({ length: 100 }).forEach(() => {
+        users.push(createRandomUser());
+      });
       return users;
-    });
-    return res.status(200).json(users);
+    }
+    const users = modelCreation();
+    function paginatedResults(model) {
+      const page = parseInt(req.query.page);
+      const limit = parseInt(req.query.limit);
+      const locale = req.query.locale;
+      const seed = req.query.seed;
+
+      const startIndex = (page - 1) * limit;
+      const endIndex = page * limit;
+
+      const results = {};
+
+      if (endIndex < model.length) {
+        results.next = {
+          page: page + 1,
+          limit,
+        };
+      }
+
+      if (startIndex > 0) {
+        results.previous = {
+          page: page - 1,
+          limit,
+        };
+      }
+      results.results = model.slice(startIndex, endIndex);
+      return results;
+    }
+    const result = paginatedResults(users);
+    return res.status(200).json(result);
   }
 }
 
