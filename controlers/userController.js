664a09270eed61d09e6430c9d9493061e69ddc4c
Proust-stack <andrey-hunter@yandex.ru>
2022-11-20 19:48:28 +0400

    routes done

--- controlers/userController.js
+++ controlers/userController.js
@@ -1,59 +1,83 @@
-const ApiError = require("../error/ApiError");
 const { faker } = require("@faker-js/faker");
+
 const createRandomUser = require("../utils/fakeGenerator");
+const ApiError = require("../error/ApiError");
 
-class UserController {
-  getData(req, res, next) {
-    function modelCreation() {
-      const locale = req.query.locale;
-      const seed = req.query.seed;
+module.exports = function UserController(req, res) {
+  const locale = req.query.locale;
+  const seed = req.query.seed;
+  const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
 
-      if (!locale || !seed) {
-        return next(
-          ApiError.badRequest(
-            "Incorrect data, fullfill all fields above, please"
-          )
-        );
-      }
-      faker.setLocale(locale);
-      faker.seed(+seed + +page);
-      const users = [];
-      Array.from({ length: 100 }).forEach(() => {
-        users.push(createRandomUser());
-      });
-      return users;
-    }
-    const users = modelCreation();
-    function paginatedResults(model) {
-      const page = parseInt(req.query.page);
-      const limit = parseInt(req.query.limit);
-      const locale = req.query.locale;
-      const seed = req.query.seed;
+  faker.setLocale(locale);
+  faker.seed(+seed + +page);
+  const users = [];
+  Array.from({ length: 100 }).forEach(() => {
+    users.push(createRandomUser());
+  });
 
-      const startIndex = (page - 1) * limit;
-      const endIndex = page * limit;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
 
-      const results = {};
+  const results = {};
 
-      if (endIndex < model.length) {
-        results.next = {
-          page: page + 1,
-          limit,
-        };
-      }
-
-      if (startIndex > 0) {
-        results.previous = {
-          page: page - 1,
-          limit,
-        };
-      }
-      results.results = model.slice(startIndex, endIndex);
-      return results;
-    }
-    const result = paginatedResults(users);
-    return res.status(200).json(result);
+  if (endIndex < users.length) {
+    results.next = {
+      page: page + 1,
+      limit,
+    };
   }
+
+  if (startIndex > 0) {
+    results.previous = {
+      page: page - 1,
+      limit,
+    };
+  }
+  results.users = users.slice(startIndex, endIndex);
+
+  // if (!locale || !seed) {
+  //   return next(
+  //     ApiError.badRequest("Incorrect data, fullfill all fields above, please")
+  //   );
+  // }
+
+  // const users = modelCreation(locale, seed);
+  // const data = paginatedResults(users, page, limit);
+  // console.log(result);
+  return res.status(200).json(results);
+};
+
+function modelCreation(locale, seed) {
+  faker.setLocale(locale);
+  faker.seed(+seed + +page);
+  const users = [];
+  Array.from({ length: 100 }).forEach(() => {
+    users.push(createRandomUser());
+  });
+
+  return users;
 }
 
-module.exports = new UserController();
+function paginatedResults(model, page, limit) {
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  const results = {};
+
+  if (endIndex < model.length) {
+    results.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    results.previous = {
+      page: page - 1,
+      limit,
+    };
+  }
+  results.users = model.slice(startIndex, endIndex);
+  return results;
+}
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,6 +2,6 @@ const Router = require("express");
 const router = new Router();
 const userController = require("../controlers/userController");
 
-router.get("/users", userController.getData);
+router.get("/users", userController);
 
 module.exports = router;
