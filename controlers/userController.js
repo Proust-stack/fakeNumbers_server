70b12480ee8b3cd32b9ac195eec43bc0378dc9bf
Proust-stack <andrey-hunter@yandex.ru>
2022-11-21 00:39:57 +0400

    done

--- controlers/userController.js
+++ controlers/userController.js
@@ -1,11 +1,10 @@
 const { faker } = require("@faker-js/faker");
-const createRandomUser = require("../utils/fakeGenerator");
 const ApiError = require("../error/ApiError");
 
 module.exports = function UserController(req, res) {
   const locale = req.query.locale;
   const seed = parseInt(req.query.seed);
-  const error = parseInt(req.query.error);
+  const error = parseFloat(req.query.error);
 
   const page = parseInt(req.query.page);
   const limit = parseInt(req.query.limit);
@@ -16,40 +15,8 @@ module.exports = function UserController(req, res) {
 
   Array.from({ length: 100 }).forEach(() => {
     const user = createRandomUser();
-    faker.helpers.maybe(makeErrors, { probability: error });
+    faker.helpers.maybe(() => makeErrors(error, user), { probability: error });
     users.push(user);
-
-    function makeErrors(error, user) {
-      for (let i = 0; i < error; i++) {
-        const keyFieldError = faker.helpers.objectKey(user);
-        const filedOfError = user[keyFieldError];
-        const arrayFromField = filedOfError.split("");
-        const typeError = faker.helpers.arrayElement(["swap", "add", "delete"]);
-        const letterOfError = faker.helpers.arrayElement(arrayOfField);
-        const pos = arrayFromField.indexOf(letterOfError);
-
-        switch (typeError) {
-          case "swap":
-            arrayFromField = [
-              ...arrayFromField.slice(0, pos),
-              arrayFromField[pos + 1],
-              arrayFromField[pos],
-              ...arrayFromField.slice[pos + 2],
-            ];
-            break;
-          case "add":
-            const randomLetter = faker.random.alpha();
-            arr.splice(pos, 0, randomLetter);
-          case "delete":
-            arr.splice(pos, 1);
-            break;
-          default:
-            break;
-        }
-      }
-      const changedFeld = arrayFromField.join("");
-      user[keyFieldError] = changedFeld;
-    }
   });
 
   const startIndex = (page - 1) * limit;
@@ -70,6 +37,52 @@ module.exports = function UserController(req, res) {
       limit,
     };
   }
+
   results.users = users.slice(startIndex, endIndex);
   return res.status(200).json(results);
 };
+
+function createRandomUser() {
+  return {
+    userId: faker.datatype.uuid(),
+    name: faker.name.fullName(),
+    address:
+      faker.address.zipCode() +
+      " " +
+      faker.address.city() +
+      ", " +
+      faker.address.streetAddress(true),
+    phone: faker.phone.number(),
+  };
+}
+
+function makeErrors(error, user) {
+  for (let i = 0; i < error; i++) {
+    const keyFieldError = faker.helpers.objectKey(user);
+    const filedOfError = user[keyFieldError];
+    let arrayFromField = filedOfError.split("");
+    const typeError = faker.helpers.arrayElement(["swap", "add", "delete"]);
+    const letterOfError = faker.helpers.arrayElement(arrayFromField);
+    const pos = arrayFromField.indexOf(letterOfError);
+    switch (typeError) {
+      case "swap":
+        arrayFromField = [
+          ...arrayFromField.slice(0, pos),
+          arrayFromField[pos + 1],
+          arrayFromField[pos],
+          ...arrayFromField.slice([pos + 2]),
+        ];
+        break;
+      case "add":
+        const randomLetter = faker.random.alpha();
+        arrayFromField.splice(pos, 0, randomLetter);
+      case "delete":
+        arrayFromField.splice(pos, 1);
+        break;
+      default:
+        break;
+    }
+    const changedFeld = arrayFromField.join("");
+    user[keyFieldError] = changedFeld;
+  }
+}
